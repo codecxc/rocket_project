fc8ceb0c5bd8898f2ff03d789e66787074a8381e
Your Name <you@example.com>
2026-04-04 18:27:21 +0300

    map

--- map.cpp
+++ map.cpp
@@ -1,2 +1,52 @@
 #include "map.h"
 
+Corner::Corner(float x, float y, float z):x(x),y(y),z(z) {}
+Corner::Corner():x(0.0f),y(0.0f),z(0.0f) {}
+
+float Corner::getX() const {
+	return x;
+}
+float Corner::getY() const {
+        return y;
+}
+float Corner::getZ() const {
+        return z;
+}
+void Corner::setX(float x) {
+        this->x=x;
+}
+void Corner::setY(float y) {
+        this->y=y;
+}
+void Corner::setZ(float z) {
+        this->z=z;
+}
+
+Cell::Cell(Corner* c1,Corner* c2,Corner* c3,Corner* c4,DefenderRocket* r=nullptr):c1(c1),c2(c2),c3(c3),c4(c4),r(r) {
+	if(r!=nullptr) this->is_have_defender_rocket=true;
+	else this->is_have_defender_rocket=false;
+}
+Cell::Cell():is_have_defender_rocket(false),r(nullptr),c1(nullptr),c2(nullptr),c3(nullptr),c4(nullptr) {}
+
+std::vector<Corner*> Cell::getCorners() const {
+	std::vector<Corners*> v;
+	v.push_back(c1);
+	v.push_back(c2);
+	v.push_back(c3);
+	v.push_back(c4);
+	return v;
+
+DefenderRocket* Cell::getDefenderRocket() const {
+	return r;
+}
+
+Corner* Cell::getCenter() const {
+	Corner* c=new Corner((c1->x+c2->x+c3->x+c4->x)/4.0,(c1->y+c2->y+c3->y+c4->y)/4.0,(c1->z+c2->z+c3->z+c4->z)/4.0));
+	return c;
+}
+
+Map::Map(Corner* c_map1,Corner* c_map2,Corner* c_map3,Corner* c_map4, std::vector<Cell*> map):c_map1(c_map1),c_map2(c_map2),c_map3(c_map3),c_map4(c_map4),map(map) {}
+Corner* Map::getCornerNumber(int num) const {
+	return map[num];
+
+}
--- map.h
+++ map.h
@@ -1,9 +1,11 @@
 #ifndef MAP_H
 #define MAP_H
+#include <vector>
 
 struct Corner {
         float x=0.0f,y=0.0f,z=0.0f;
 	Corner(float x, float y,float z);
+	Corner();
 	float getX() const;
 	float getY() const;
 	float getZ() const;
@@ -30,9 +32,8 @@ class Map {
 		std::vector<Cell*> map;
 		Corner* c_map1,*c_map2,*c_map3,*c_map4;
 	public:
-		Map(Corner* c1,Corner* c2,Corner* c3,Corner* c4, std::vector<Cell*> map);
-		Map();
-		Corner* getCenterNumber(int num) const;
+		Map(Corner* c_map1,Corner* c_map2,Corner* c_map3,Corner* c_map4, std::vector<Cell*> map);
+		Corner* getCornerNumber(int num) const;
 };
 
 
